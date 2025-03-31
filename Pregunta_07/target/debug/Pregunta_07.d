377afe50f52429be4c03ae794ceea080536bc53a
DeivisMGF <maycolguivar11@gmail.com>
2025-03-31 13:26:10 -0500

    creacion de Pregunta_07

---
+++ Pregunta_07/target/debug/Pregunta_07.d
@@ -0,0 +1,1 @@
+C:\Users\mayco\Downloads\Ciclo\ 2025\ I\ADA\Talleres\Taller\ 1\Pregunta_07\target\debug\Pregunta_07.exe: C:\Users\mayco\Downloads\Ciclo\ 2025\ I\ADA\Talleres\Taller\ 1\Pregunta_07\src\main.rs
---
+++ Pregunta_07/target/debug/deps/Pregunta_07.d
@@ -0,0 +1,5 @@
+C:\Users\mayco\Downloads\Ciclo 2025 I\ADA\Talleres\Taller 1\Pregunta_07\target\debug\deps\Pregunta_07.exe: src\main.rs
+
+C:\Users\mayco\Downloads\Ciclo 2025 I\ADA\Talleres\Taller 1\Pregunta_07\target\debug\deps\Pregunta_07.d: src\main.rs
+
+src\main.rs:
